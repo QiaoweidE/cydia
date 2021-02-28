4137c1ffd963a492bde002b9cef49b7970206715
K2D <54195182+QiaoweidE@users.noreply.github.com>
2021-02-28 13:35:48 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,62 +1,7 @@
 #!/bin/sh
-function building() {
-  echo "[" > all.pkgs
-if [[ -e compatity.txt ]]; then
-    compatity=$(cat compatity.txt)
-fi
-
-for i in debfiles/*.deb
-do
-   debInfo=`dpkg -f $i`
-   pkg=`echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`
-
-   section=`echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`
-   section="${section//'"'/\\\"}"
-
-   name=`echo "$debInfo" | grep "Name: " | cut -c 7- | tr -d "\n\r"`
-   name="${name//'"'/\\\"}"
-
-   vers=`echo "$debInfo" | grep "Version: " | cut -c 10- | tr -d "\n\r"`
-   vers="${vers//'"'/\\\"}"
-
-   author=`echo "$debInfo" | grep "Author: " | cut -c 9- | tr -d "\n\r"`
-   author="${author//'"'/\\\"}"
-
-   depends=`echo "$debInfo" | grep "Depends: " | cut -c 10- | tr -d "\n\r"`
-   depends="${depends//'"'/\\\"}"
-
-   description=`echo "$debInfo" | grep "Description: " | cut -c 14- | tr -d "\n\r"`
-   description="${description//'"'/\\\"}"
-
-   arch=`echo "$debInfo" | grep "Architecture: " | cut -c 15- | tr -d "\n\r"`
-   arch="${arch//'"'/\\\"}"
-
-   size=$(du -b $i | cut -f1)
-   time=$(date +%s -r $i)
-    
-   echo '{"Name":"'$name'","Version":"'$vers'","Section":"'$section'","Package":"'$pkg'","Author":"'$author'","Depends":"'$depends'","Descript":"'$description'","Arch":"'$arch'","Size":"'$size'","Time":"'$time'000"},' >> all.pkgs
-#Building to json done==============
-  leng=${#pkg}
-  leng=`expr $leng + 1`
-  exists=`echo "$compatity" | grep "$pkg " | cut -c "$leng"- | tr -d "\n\r"`
-  if [[ -z $exists ]]; then
-     echo "$pkg ($name)? "
-     read tmp
-     echo "$pkg $tmp" >> compatity.txt;
-  fi
-done
-
-echo "{}]" >> all.pkgs
-}
-echo "------------------"
-echo "Building Packages...."
-apt-ftparchive packages ./debfiles > ./Packages;
+apt-ftparchive packages ./debfiles/ > ./Packages;
 #sed -i -e '/^SHA/d' ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
-echo "------------------"
-echo "Building Release...."
-printf "Origin: Apple Cydia & Jailbreak\nLabel: Cydia And Jailbreak\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Apple Phong\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
-
-echo "------------------"
-echo "Done!"
+printf "Origin: QiaoweidE\nLabel: Weidevn\nSuite: stable\nVersion: 1.0\nCodename: QiaoweidE\nArchitecture: iphoneos-arm\nComponents: main\nDescription: QiaoweidE Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+ls ./debfiles/ -t | grep '.deb' | perl -e 'use JSON; @in=grep(s/\n$//, <>); $count=0; foreach $fileNow (@in) { $fileNow = "./debfiles/$fileNow"; $size = -s $fileNow; $debInfo = `dpkg -f $fileNow`; $section = `echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`; $name= `echo "$debInfo" | grep "Name: " | cut -c 7- | tr -d "\n\r"`; $version= `echo "$debInfo" | grep "Version: " | cut -c 10- | tr -d "\n\r"`; $package= `echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`; $time= `date -r $fileNow +%s | tr -d "\n\r"`; @in[$count] = {section=>$section, package=>$package, version=>$version, size=>$size+0, time=>$time+0, name=>$name}; $count++; } print encode_json(\@in)."\n";' > all.packages;
 exit 0;
